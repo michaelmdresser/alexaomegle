570c7d7a18d9b2587ed4ccc79e40f26b3d7bca79
Michael Dresser <michaelmdresser@gmail.com>
2016-11-18 23:56:36 -0700

    added response builder

--- runOmegle.py
+++ runOmegle.py
@@ -1,19 +1,26 @@
+from pyomegle import OmegleHandler, OmegleClient
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+#        'card': {
+#            'type': 'Simple',
+#            'title': "SessionSpeechlet - " + title,
+#            'content': "SessionSpeechlet - " + output
+#        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+
 def lambda_handler(event, context):
     if event["request"]["intent"]["name"] == "StartConversationIntent":
-        return {
-          "version": "1.0",
-          "response": {
-            "outputSpeech": {
-              "type": "PlainText",
-              "text": "hi Michael"
-            },
-            "reprompt": {
-              "outputSpeech": {
-                "type": "PlainText",
-                "text": ""
-              }
-            },
-            "shouldEndSession": True
-          },
-          "sessionAttributes": {}
-        }
+        return {}
